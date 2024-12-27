bee8aec9b30e99caedd93af516c31cb5419bc678
Deepak Kumar <deepak.kumar2@omf.com>
2024-12-27 16:56:21 -0600

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { EmployeeComponent } from './employee/employee.component';
 //import { RouterOutlet } from '@angular/router';
 //import { EmployeeComponent } from './employee/employee.component';
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
+  imports:  [EmployeeComponent],
   template:   `
               <div>
                 <h1>{{title ? title : 'No title' }}</h1>  
+                <my-employee></my-employee> 
                 </div>
-
+                 
                 `,
 styleUrls:  ['./app.component.scss']
 
