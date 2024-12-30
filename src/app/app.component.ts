e3ebe8dd5f64b8f66cf4789dba223b712506d14f
Deepak Kumar <deepak.kumar2@omf.com>
2024-12-30 09:36:43 -0600

    first project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,59 @@
 import { Component } from '@angular/core';
 import { EmployeeComponent } from './employee/employee.component';
-//import { RouterOutlet } from '@angular/router';
-//import { EmployeeComponent } from './employee/employee.component';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { EmployeeListComponent } from './employee/employeeList.component';
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
-  imports:  [EmployeeComponent],
+  imports:  [CommonModule,FormsModule,EmployeeComponent,EmployeeListComponent],
   templateUrl: './app.component.html',
 styleUrls:  ['./app.component.scss']
 
- 
-  
- 
+
 })
 export class AppComponent {
-  title = 'Employee Details';
+  title: string  = 'Employee Details';
+  imagePath: string = 'pargim-logo-1.png' ;
+  isDisabled: boolean = false;
+  classesToApply: string = 'italicsClass boldClass';
+  applyBoldClass: boolean = true;
+  applyItalicsClass: boolean  = false ;
+  isBold: boolean = true;
+  isItalic: boolean = true;
+  fontSize: number = 30;
+  firstName: string  = 'Deepak' ;
+  lastName: string = 'kumar' ;
+  badHtml: string = 'Hello <script>alert("Hacked");</script> World';
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  addClasses() {
+    let classes = {
+      boldClass: this.applyBoldClass,
+      italicsClass: this.applyItalicsClass
+    };
+    return classes;
+  }
+  addStyles() {
+    let styles = {
+        'font-size.px' : this.fontSize,
+        'font-style' : this.isItalic ? 'italic'  : 'normal' ,
+        'font-weight' : this.isBold ? 'bold' :'normal',
+    }
+        return styles;
+  }
+  onClick(): void{
+    console.log('Button Clicked');
+  }
+  name: string = 'Tom';
+  // Method to handle the input event
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement; // Cast the event target
+    this.name = input.value; // Update the name property
+  }
 }
 // <my-employee></my-employee> 
--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -1,17 +1,20 @@
-
 import { Component , CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'employee',
-    templateUrl: './employee.component.html' ,
-     styleUrl: './employee.component.scss',
+    selector: 'list-employee',
+    templateUrl: './employeeList.component.html' ,
+     styleUrl: './employee.componentList.scss',
     imports: [CommonModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
    
 })
 
-export class EmployeeComponent {
+export class EmployeeListComponent {
+    employees:any[] = [
+        {code: 'emp101', name: 'Tom' , gender: 'Male', salary: 5500 },
+        {code: 'emp102', name: 'Alex' , gender: 'Male', salary: 5700 }
+    ];
     columnSpan: number = 2;
     firstName: string = 'Deepak';
     lastName: string = 'Kumar' ;
